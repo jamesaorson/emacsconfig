8a56a200444eb6cdf9e38a939d7dc679a50d17d3
James Orson <jamesaorson@gmail.com>
2023-08-18 13:21:01 +0000

    Move package installs to init.el

--- config/init.el
+++ config/init.el
@@ -1,34 +1,41 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(defun configure-column-mode ()
+  (setq column-number-mode t))
+
 (defun configure-line-mode ()
   (global-display-line-numbers-mode)
   (setq display-line-numbers-type 'relative)
   (global-hl-line-mode))
 
-(defun configure-column-mode ()
-  (setq column-number-mode t))
+(defun configure-weird-behaviors ()
+  (setq vc-follow-symlinks t))
 
-(defun configure-color-theme! ()
-  (custom-set-variables
-   ;; custom-set-variables was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
-   '(ansi-color-faces-vector
-     [default default default italic underline success warning error])
-   '(ansi-color-names-vector
-     ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
-   '(custom-enabled-themes '(manoj-dark))
-  '(package-selected-packages
-    '(clojure-mode tabbar session pod-mode muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode folding eproject diminish csv-mode company color-theme-modern browse-kill-ring boxquote bm bar-cursor apache-mode magit)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ))
+(defun install-packages (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not. Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
 
-(configure-color-theme!)
 (configure-line-mode)
 (configure-column-mode)
+(configure-weird-behaviors)
+(install-packages
+ 'clojure-mode
+ 'magit
+ 'rainbow-mode
+ 'terraform-mode
+ )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
