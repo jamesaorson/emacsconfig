90b28f172cf618fd5b682184f29b11bd5fa9937c
James Orson <jamesaorson@gmail.com>
2023-08-18 13:13:26 +0000

    Properly check for installed packages. Add rainbow-mode.

--- config/early-init.el
+++ config/early-init.el
@@ -1,18 +1,26 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
 (defun configure-weird-behaviors ()
   (setq vc-follow-symlinks t))
 
-(defun install-packages ()
-  (unless
-      (and
-       (package-installed-p 'magit)
-       (package-installed-p 'clojure-mode))
-    (require 'package)
-    (add-to-list 'package-archives
-		 '("melpa" . "https://melpa.org/packages/") t)
-    (package-refresh-contents)
-    (package-install 'magit)
-    (package-install 'clojure-mode)))
+(defun install-packages (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not. Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
 
 (configure-weird-behaviors)
-(install-packages)
+;; make sure to have downloaded archive description.
+;; Or use package-archive-contents as suggested by Nicolas Dudebout
+(unless package-archive-contents
+  (package-refresh-contents))
+(install-packages 'clojure-mode 'magit 'rainbow-mode)
 
--- config/init.el
+++ config/init.el
@@ -1,21 +1,24 @@
 (defun configure-line-mode ()
   (global-display-line-numbers-mode)
+  (setq display-line-numbers-type 'relative)
   (global-hl-line-mode))
 
 (defun configure-column-mode ()
-    (setq column-number-mode t))
+  (setq column-number-mode t))
 
 (defun configure-color-theme! ()
   (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes '(manoj-dark)))
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(ansi-color-faces-vector
+     [default default default italic underline success warning error])
+   '(ansi-color-names-vector
+     ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+   '(custom-enabled-themes '(manoj-dark))
+  '(package-selected-packages
+    '(clojure-mode tabbar session pod-mode muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode folding eproject diminish csv-mode company color-theme-modern browse-kill-ring boxquote bm bar-cursor apache-mode magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
