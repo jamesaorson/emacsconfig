6655b6eac14ddb894dd7db710c962d0ddc42dc86
James Orson <jamesaorson@gmail.com>
2023-08-25 17:55:19 +0000

    Use brew to install emacs on linux

--- scripts/setup.linux.bash
+++ scripts/setup.linux.bash
@@ -4,8 +4,9 @@ set -euo pipefail
 
 cd $(dirname ${BASH_SOURCE[0]})/..
 
+brew install emacs
+brew services start emacs
 sudo apt-get install -qy \
-     emacs \
      texlive-full
 
 ./scripts/setup.common.bash
