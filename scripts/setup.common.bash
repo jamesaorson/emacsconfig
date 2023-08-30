6d43d4ba1d4774ecae5b42d4c46bcbeb2cc23c76
James Orson <jamesaorson@gmail.com>
2023-08-30 13:31:22 -0700

    Move directory for config files to match reality

--- scripts/setup.common.bash
+++ scripts/setup.common.bash
@@ -13,7 +13,7 @@ mkdir -p ${CONFIG_DIR}
 ln -s -f $(pwd)/bin/emacs-ssh ${BIN_DIR}/emacs-ssh
 
 for file in "init.el" "early-init.el"; do
-    ln -s -f $(pwd)/config/${file} ${CONFIG_DIR}/${file}
+    ln -s -f $(pwd)/emacs.d/${file} ${CONFIG_DIR}/${file}
 done
 
 # NOTE: --user avoids an error: https://emacs.stackexchange.com/questions/34022/error-initialization-user-has-no-home-directory
