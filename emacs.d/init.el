7fbead9152e38ffebef84f204e3897ef7e7afba2
James Orson <jamesaorson@gmail.com>
2023-09-29 07:16:46 -0700

    Set new color theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -322,7 +322,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(manoj-dark))
+ '(custom-enabled-themes '(modus-vivendi-tinted))
  '(inhibit-startup-screen t)
  '(package-selected-packages nil))
 (custom-set-faces
