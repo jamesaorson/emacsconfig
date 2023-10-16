e1e82b62f8d8d3cab30384eff409b66c780ca87c
James Orson <jamesaorson@gmail.com>
2023-10-16 07:37:23 -0700

    Update theme and minimize problems

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,7 +4,7 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)     ;; changes all yes/no questions to y/n type
 
-(set-face-attribute 'default nil :font "FiraMono Nerd Font")
+;; (set-face-attribute 'default nil :font "FiraMono Nerd Font")
 
 ;;; Emacs Load Path
 (add-to-list 'load-path "~/.emacs.d/packages/")
@@ -345,7 +345,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi-tinted))
+ '(custom-enabled-themes '(modus-vivendi))
  '(inhibit-startup-screen t)
  '(package-selected-packages nil))
 (custom-set-faces
@@ -357,6 +357,4 @@
 
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
-
-(treemacs)
 
