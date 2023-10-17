3d2cc9310aa0e5d6ff855e211360e5a324e24cb3
James Orson <jamesaorson@gmail.com>
2023-10-17 04:53:57 +0000

    Add minibuffer completion

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -279,6 +279,8 @@
    ;; Mouse yank commands yank at point instead of at click.
    mouse-yank-at-point t)
 
+  (ido-mode t)
+
   ;; Sets up exec-path-from shell
   ;; https://github.com/purcell/exec-path-from-shell
   (setup 
