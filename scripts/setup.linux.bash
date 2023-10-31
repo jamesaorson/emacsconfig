69adec484cbfdbd102cbabcbb1ce8695df35b0d3
James Orson <jamesaorson@gmail.com>
2023-10-31 19:57:47 +0000

    Fix build process and minimize sudo use

--- scripts/setup.linux.bash
+++ scripts/setup.linux.bash
@@ -2,83 +2,92 @@
 
 set -euox pipefail
 
-sudo apt-get install -qy \
-     build-essential \
-     gcc-10 \
-     graphviz \
-     libacl1-dev \
-     libc6-dev \
-     libdbus-1-dev \
-     libgccjit-10-dev \
-     libgconf2-dev \
-     libgif-dev \
-     libgnutls28-dev \
-     libgpm-dev \
-     libgtk-3-dev \
-     libharfbuzz-bin \
-     libharfbuzz-dev \
-     libice-dev \
-     libjansson-dev \
-     libjpeg-dev \
-     libm17n-dev \
-     libmagickcore-dev \
-     libmagickwand-dev \
-     libncurses5-dev \
-     libotf-dev \
-     libpng-dev \
-     librsvg2-dev \
-     libsm-dev \
-     libsystemd-dev \
-     libtiff-dev \
-     libtinfo-dev \
-     libwebkit2gtk-4.0-dev \
-     libwebp-dev \
-     libx11-dev \
-     libxaw3dxft8-dev \
-     libxaw7-dev \
-     libxext-dev \
-     libxft-dev \
-     libxft2 \
-     libxi-dev \
-     libxmu-dev \
-     libxmuu-dev \
-     libxpm-dev \
-     libxrandr-dev \
-     libxt-dev \
-     libxtst-dev \
-     libxv-dev \
-     libz-dev \
-     texlive-full \
-     xaw3dg-dev \
-     zlib1g-dev
+if sudo -v; then
+    sudo apt-get install -qy \
+	 build-essential \
+	 gcc-10 \
+	 graphviz \
+	 libacl1-dev \
+	 libc6-dev \
+	 libdbus-1-dev \
+	 libgccjit-10-dev \
+	 libgconf2-dev \
+	 libgif-dev \
+	 libgnutls28-dev \
+	 libgpm-dev \
+	 libgtk-3-dev \
+	 libharfbuzz-bin \
+	 libharfbuzz-dev \
+	 libice-dev \
+	 libjansson-dev \
+	 libjpeg-dev \
+	 libm17n-dev \
+	 libmagickcore-dev \
+	 libmagickwand-dev \
+	 libncurses5-dev \
+	 libotf-dev \
+	 libpng-dev \
+	 librsvg2-dev \
+	 libsm-dev \
+	 libsystemd-dev \
+	 libtiff-dev \
+	 libtinfo-dev \
+	 libwebkit2gtk-4.0-dev \
+	 libwebp-dev \
+	 libx11-dev \
+	 libxaw3dxft8-dev \
+	 libxaw7-dev \
+	 libxext-dev \
+	 libxft-dev \
+	 libxft2 \
+	 libxi-dev \
+	 libxmu-dev \
+	 libxmuu-dev \
+	 libxpm-dev \
+	 libxrandr-dev \
+	 libxt-dev \
+	 libxtst-dev \
+	 libxv-dev \
+	 libz-dev \
+	 texlive-full \
+	 xaw3dg-dev \
+	 zlib1g-dev
 
-TREE_SITTER=libtree-sitter-dev
-if [[ $(apt-cache search ${TREE_SITTER}) != "" ]]; then
+    TREE_SITTER=libtree-sitter-dev
+    if [[ $(apt-cache search ${TREE_SITTER}) != "" ]]; then
 	sudo apt-get install -qy ${TREE_SITTER}
+    fi
 fi
 
 cd $(dirname ${BASH_SOURCE[0]})
 
+LOCAL_DIR=${HOME}/.local
+
 cd ../src
 ./autogen.sh
 ./configure \
-  --with-cairo \
-  --with-gconf \
-  --with-gnutls \
-  --with-imagemagick \
-  --with-json \
-  --with-modules \
-  --with-native-compilation \
-  --with-tree-sitter=ifavailable \
-  --with-x \
-  --with-xft \
-  --with-xml2 \
-  --with-xwidgets \
-  --with-x-toolkit=gtk3 \
-  CC=gcc-10
+    --prefix=${LOCAL_DIR} \
+    --with-cairo \
+    --with-gconf \
+    --with-gnutls \
+    --with-imagemagick \
+    --with-json \
+    --with-modules \
+    --with-native-compilation \
+    --with-tree-sitter=ifavailable \
+    --with-x \
+    --with-xft \
+    --with-xml2 \
+    --with-xwidgets \
+    --with-x-toolkit=gtk3 \
+    CC=gcc-10
 
-make -j8
-sudo make install
+make -j16
+make install
 cd ../scripts
 ./setup.common.bash
+
+if [[ $(whoami) == "ursinia" ]]; then
+  sudo ln -s -f ${LOCAL_DIR}/bin/emacs /usr/local/bin/emacs
+fi
 
